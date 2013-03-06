46ffab72fe3a59c039d1c8b0ef3d8ffc931a2509
knavero <knavero@knavero-AO756>
2013-03-06 11:20:42 -0800

    first commit

---
+++ src/Sorts.java
@@ -0,0 +1,38 @@
+
+public class Sorts {
+
+   //O(N^2) routines:
+   /*
+    * Description: Sorts the list of size elements contained in arr using the selection sort algorithm
+    */
+   public static <Type extends Comparable<? super Type>> void selectionSort(Type[] arr, int size) {
+   }
+   
+   /*
+    * Description: Sorts the list of size elements contained in arr using the bubble sort algorithm
+    */
+   public static <Type extends Comparable<? super Type>> void bubbleSort(Type[] arr, int size) {
+   }
+   
+   /*
+    * Description: Sorts the list of size elements contained in arr using the insertion sort algorithm
+    */
+   public static <Type extends Comparable<? super Type>> void insertionSort(Type[] arr, int size) {
+   }
+   
+   //O(NlogN) routines:
+   /*
+    * Description: Sorts the list of size elements contained in arr using the merge sort algorithm 
+    */
+   public static <Type extends Comparable<? super Type>> void mergeSort(Type[] arr, int size) {
+   }
+   
+   /*
+    * Description: Sorts the list of size elements contained in arr using the quick sort algorithm with
+    * median-of-three pivot and rearrangement of the three elements (as done in class)
+    */
+   public static <Type extends Comparable<? super Type>> void quickSort(Type[] arr, int size) {
+      //need handout for this. Mine's missing.
+   }
+
+}
